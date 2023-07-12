b11e7351e378f807564ba06e10352de445df8679
Polina <deadvinny1@gmail.com>
2023-07-12 17:53:28 +0300

    добавлена задача 22

---
+++ task22/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+
+
+void Square(int numN)
+{
+    Console.Write($"{numN} -> ");
+    for (int i = 1; i <= numN; i++)
+    {
+       if (i < numN) Console.Write($"{i * i}, " );
+       else Console.Write($"{i * i}." );
+    }
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int inputNumber = Convert.ToInt32(Console.ReadLine());
+    return inputNumber;
+}
+
+int numN = GetInfo("Введите число: ");
+Square(numN);
