a601688e45eb359aae378f342c3115679d11f866
Polina <deadvinny1@gmail.com>
2023-07-12 15:52:50 +0300

    Добавлена задача

---
+++ task0/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int DefQuarter(int xPoint, int yPoint)
+{
+    if (xPoint > 0 & yPoint > 0) return 1;
+    if (xPoint < 0 & yPoint > 0) return 2;
+    if (xPoint < 0 & yPoint < 0) return 3;
+    if (xPoint > 0 & yPoint < 0) return 4;
+    return 0;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+
+
+int xPoint = GetInfo("Введите координату х: ");
+int yPoint = GetInfo("Введите координату y: ");
+Console.WriteLine($"Это {DefQuarter(xPoint, yPoint)} четверть!");
+
